efa1a968e701e2e481eca5a16c4a2fcf4a12ec2e
Can Kattw <kattwinkel@w11k.de>
2016-06-06 10:16:30 +0200

    add translate example to page hello-ionic

--- app/pages/hello-ionic/hello-ionic.ts
+++ app/pages/hello-ionic/hello-ionic.ts
@@ -1,8 +1,10 @@
 import {Page} from 'ionic-angular';
+import {TranslatePipe} from "ng2-translate/ng2-translate";
 
 
 @Page({
-  templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+    templateUrl: 'build/pages/hello-ionic/hello-ionic.html',
+    pipes: [TranslatePipe]
 })
 export class HelloIonicPage {
   constructor() {
