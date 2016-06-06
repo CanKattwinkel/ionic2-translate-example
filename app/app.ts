3bdaa824cb2120096d86c883de31ce7693ffe9a4
Can Kattw <kattwinkel@w11k.de>
2016-06-06 10:15:15 +0200

    configuration of ng2-translate

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,20 @@ import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
 
+import {provide} from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
+import {TranslateService, TranslatePipe, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
+
 
 @App({
   templateUrl: 'build/app.html',
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
+  providers: [provide(TranslateLoader, {
+    useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n/', '.json'),
+    deps: [Http]
+  }),
+    TranslateService],
+  pipes: [TranslatePipe]
 })
 class MyApp {
   @ViewChild(Nav) nav: Nav;
@@ -18,7 +28,8 @@ class MyApp {
 
   constructor(
     private platform: Platform,
-    private menu: MenuController
+    private menu: MenuController,
+    private  translate: TranslateService
   ) {
     this.initializeApp();
 
@@ -27,6 +38,7 @@ class MyApp {
       { title: 'Hello Ionic', component: HelloIonicPage },
       { title: 'My First List', component: ListPage }
     ];
+    this.translateConfig();
   }
 
   initializeApp() {
@@ -35,6 +47,17 @@ class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  translateConfig() {
+    var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+    userLang = /(fr|en|de)/gi.test(userLang) ? userLang : 'en';
+
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang('en');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use(userLang);
   }
 
   openPage(page) {
